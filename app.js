fccaa9471266f6ed346979f2cf1630ee5b1c5f38
Hasan1284 <hasansevindik1284@gmail.com>
2020-05-02 17:26:02 +0200

    node js e giris denemeleri

--- app.js
+++ app.js
@@ -1,35 +1,54 @@
-// Functions
+// // Functions
 
-function yasHesapla(dogumYili){
-    return 2020 - dogumYili;
-}
+// function yasHesapla(dogumYili){
+//     return 2020 - dogumYili;
+// }
 
-let val = yasHesapla(1983);
+// let val = yasHesapla(1983);
 
-let ageAda = yasHesapla(2012);
-let ageYigit = yasHesapla(2010);
-let ageCinar = yasHesapla(2017);
+// let ageAda = yasHesapla(2012);
+// let ageYigit = yasHesapla(2010);
+// let ageCinar = yasHesapla(2017);
 
-console.log(ageAda);
-console.log(ageYigit);
-console.log(ageCinar);
+// console.log(ageAda);
+// console.log(ageYigit);
+// console.log(ageCinar);
 
-function EmekliligeKacYilKaldi(dogumYili,isim){
-    let yas = yasHesapla(dogumYili);
-    let emeklilik = 65 - yas;
+// function EmekliligeKacYilKaldi(dogumYili,isim){
+//     let yas = yasHesapla(dogumYili);
+//     let emeklilik = 65 - yas;
 
-    if(emeklilik>0){
-        console.log(`${isim}emekli olmanıza ${emeklilik} yıl kaldı`)
+//     if(emeklilik>0){
+//         console.log(`${isim}emekli olmanıza ${emeklilik} yıl kaldı`)
 
-    }else{
-        console.log('zaten emekli oldunuz');
-    }
-}
+//     }else{
+//         console.log('zaten emekli oldunuz');
+//     }
+// }
 
 
-EmekliligeKacYilKaldi(2012,'ada');
-EmekliligeKacYilKaldi(2010,'yigit');
-EmekliligeKacYilKaldi(1950,'cınar');
-//burada fonksiyonlar konusuna giriş yapıldı
+// EmekliligeKacYilKaldi(2012,'ada');
+// EmekliligeKacYilKaldi(2010,'yigit');
+// EmekliligeKacYilKaldi(1950,'cınar');
+// //burada fonksiyonlar konusuna giriş yapıldı
 
-/// deneme
+// /// deneme
+
+//nodejs uygulamaları
+
+
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 5000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Hasan');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
